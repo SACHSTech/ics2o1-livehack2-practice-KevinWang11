a09eaa0466e62954f3633646e2be13c219aeedca
KevinWang1122 <yukai.wang23@ycdsbk12.ca>
2021-02-15 23:43:59 +0000

    Finished Code

--- problem1.py
+++ problem1.py
@@ -1,1 +1,18 @@
+side_1 = int(input("Please enter the value of side 1: "))
+side_2 = int(input("Please enter the value of side 2: "))
+side_3 = int(input("Please enter the value of side 3: "))
+
+square_1 = side_1**2
+square_2 = side_2**2
+square_3 = side_3**2
+
+if (square_1+square_2 == square_3) or (square_1+square_3 == square_2) or (square_2+square_3 == square_1):
+    print("A triangle with lengths " + str(side_1) + ", " +
+    str(side_2) + ", and " + str(side_3) + " forms a right-angled triangle.")
+else:
+    print("A triangle with lengths " + str(side_1) + ", " +
+    str(side_2) + ", and " + str(side_3) + " does not form a right-angled triangle.")
+
+
+
 
--- problem2.py
+++ problem2.py
@@ -1,1 +1,12 @@
+mark = int(input("Please enter your average here: "))
+earnings = int(input("Please enter your earnings here: "))
+
+if mark >= 80 and earnings >= 500:
+  print("You can go to Europe in the summer!")
+
+elif mark >= 80 and earnings < 500:  
+  print("You can go to California in the summer!")
+
+else:
+  print("You do not get to go anywhere in the summer.")
 
